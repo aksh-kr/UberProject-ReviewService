e2af22e74e285d9ee54d5e1a68b8833ab5d23fdd
Akash Kumar <kr.akash149@gmail.com>
2024-04-14 17:07:54 +0530

    Added Review Repository using Jpa Repository

---
+++ src/main/java/com/example/UberReviewService/services/ReviewService.java
@@ -0,0 +1,39 @@
+package com.example.UberReviewService.services;
+
+import com.example.UberReviewService.models.Review;
+import com.example.UberReviewService.repositories.ReviewRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ReviewService implements CommandLineRunner {
+
+    private ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("***********************");
+        Review r = Review.builder()
+                .content("Amazin ride quality")
+                .rating(4.0)
+                .build(); // code to create plain java object
+        System.out.println(r);
+        reviewRepository.save(r); // this code executes sql query
+
+        List<Review> reviewList = reviewRepository.findAll();
+
+        for(Review review : reviewList) {
+            System.out.println(review.getContent());
+        }
+
+        reviewRepository.deleteById(2L);
+    }
+}
