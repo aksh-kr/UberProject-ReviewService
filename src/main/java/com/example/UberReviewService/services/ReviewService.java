af80820663fad3fd3a83e6aaf908d6dacbbfb3ef
Akash Kumar <kr.akash149@gmail.com>
2024-04-17 00:07:42 +0530

    Added booking associated to review

--- src/main/java/com/example/UberReviewService/services/ReviewService.java
+++ src/main/java/com/example/UberReviewService/services/ReviewService.java
@@ -1,10 +1,13 @@
 package com.example.UberReviewService.services;
 
+import com.example.UberReviewService.models.Booking;
 import com.example.UberReviewService.models.Review;
+import com.example.UberReviewService.repositories.BookingRepository;
 import com.example.UberReviewService.repositories.ReviewRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Book;
 import java.util.Date;
 import java.util.List;
 
@@ -12,21 +15,33 @@ import java.util.List;
 public class ReviewService implements CommandLineRunner {
 
     private ReviewRepository reviewRepository;
+    private BookingRepository bookingRepository;
 
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(ReviewRepository reviewRepository, BookingRepository bookingRepository) {
         this.reviewRepository = reviewRepository;
+        this.bookingRepository = bookingRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
         System.out.println("***********************");
+
         Review r = Review.builder()
                 .content("Amazin ride quality")
                 .rating(4.0)
                 .build(); // code to create plain java object
+
+        Booking b = Booking
+                .builder()
+                .review(r)
+                .endTime(new Date())
+                .build();
+
+        bookingRepository.save(b);
+
         System.out.println(r);
-        reviewRepository.save(r); // this code executes sql query
+//        reviewRepository.save(r); // this code executes sql query
         System.out.println(r.getId());
 
         List<Review> reviewList = reviewRepository.findAll();
